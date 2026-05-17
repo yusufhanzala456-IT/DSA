ddfc73e1494d3702690b82c030c41283d1eff729
YusufHanzala <yusufhanzala456@gmail.com>
2026-05-17 15:05:17 +0530

    fever

---
+++ fever.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class fever{
+    public static void main(String[] args) {
+        double temp = 103.5;
+        if(temp > 100)
+            System.out.println("Have fever ");
+        else
+            System.out.println("Don't have fever");
+
+    }
+}
