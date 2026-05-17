95157bae356e52c62b23a85c6bf8f5ada6664858
YusufHanzala <yusufhanzala456@gmail.com>
2026-05-17 15:37:50 +0530

    leapyear

---
+++ leapyear.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class leapyear {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Years : ");
+        int year = sc.nextInt();
+        if(year % 4 == 0 && year % 100 != 0)
+            System.out.println("Leap year");
+        else if(year % 4 == 0 && year % 100 == 0 && year % 400 == 0)
+             System.out.println("Leap year");
+        else
+             System.out.println("Not Leap year");
+    }
+}
