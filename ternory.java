fd8d4cdcfdfd9900231239c7da24d6a723699219
YusufHanzala <yusufhanzala456@gmail.com>
2026-05-17 15:26:55 +0530

    ternory

---
+++ ternory.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class ternory {
+    public static void main(String [] args){
+        System.out.print("What is the value of X and Y ");
+         
+        int a = 60, b = 40;
+        boolean x = (a < b) ? true : false;
+        System.out.println(x);
+        int y = (a > b) ? a : b;
+        System.out.print(y);
+    }
+    
+}
