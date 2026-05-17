f6246bf1459def75b8f5f6c57d5e2c077216ced8
YusufHanzala <yusufhanzala456@gmail.com>
2026-05-17 14:51:29 +0530

    positivenegative

---
+++ positiveOrnegative.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class positiveOrnegative{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number ");
+        int x = sc.nextInt();
+        if(x>=0)
+            System.out.println("Number is positive ");
+        else
+            System.out.println("number is negative");
+
+    }
+}
