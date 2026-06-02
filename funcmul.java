e075080bb573aa7fefd8a95268b82c8f7e6ad9bb
YusufHanzala <yusufhanzala456@gmail.com>
2026-06-02 11:22:34 +0530

    multiplication by usnig function call

---
+++ funcmul.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class funcmul{
+    public static int mul(int a, int b){
+        int product = a * b;
+        return product;
+    }
+    public static void main(String[] args){
+    
+       int product = mul(2 , 3);
+       System.out.println(product);
+    }
+}
