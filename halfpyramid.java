b404d864846db2178ce916badacf0c2aaf33af3d
YusufHanzala <yusufhanzala456@gmail.com>
2026-06-05 10:12:08 +0530

    Half_Pyramid

---
+++ halfpyramid.java
@@ -0,0 +1,17 @@
+public class halfpyramid {
+    public static void pyramid(int n){
+        for(int i = 1 ; i <= n ; i++){
+            for(int j =1; j<=n-i; j++){
+                System.out.print(" ");
+            }
+            for(int j = 1; j<= i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+public static void main(String args[]){
+    pyramid(5);
+}
+    
+}
