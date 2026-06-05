f026e66f607ed76f8e0e2ed46ccac2f49ba2245d
YusufHanzala <yusufhanzala456@gmail.com>
2026-06-05 10:41:31 +0530

    Floyed Triangle

---
+++ printpattern.java
@@ -0,0 +1,16 @@
+public class printpattern {
+    int counter = 1;
+    public static void pattern_counter(int n){
+        int counter = 1;
+        for(int i = 1 ; i <= n; i++){
+            for(int j = 1; j<=i; j++){
+                System.out.print(counter+" ");
+                counter++;
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String args[]){
+        pattern_counter(5);   
+    }
+}
