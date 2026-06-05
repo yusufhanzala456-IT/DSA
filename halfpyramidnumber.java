f6c5454390436328ffb97486a72c43a18037383f
YusufHanzala <yusufhanzala456@gmail.com>
2026-06-05 10:30:10 +0530

    Inverted_Half_Pyramid_withNumbers

---
+++ halfpyramidnumber.java
@@ -0,0 +1,13 @@
+public class halfpyramidnumber {
+    public static void inverted_half_pyramid_withnumbers(int n){
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n-i+1; j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String args[]){
+        inverted_half_pyramid_withnumbers(5);
+    }
+}
