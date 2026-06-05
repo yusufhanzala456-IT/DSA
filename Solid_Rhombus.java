6e5fffc229b5ba62a292593687278af452acc5aa
YusufHanzala <yusufhanzala456@gmail.com>
2026-06-05 11:37:21 +0530

    Solid Rhombus Pattern

---
+++ Solid_Rhombus.java
@@ -0,0 +1,16 @@
+public class Solid_Rhombus {
+    public static void rhombus_patt(int n){
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<= n-i; j++){
+                System.out.print(" ");
+            }
+            for(int j=1; j<=n; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String args[]){
+        rhombus_patt(5);
+    }
+}
