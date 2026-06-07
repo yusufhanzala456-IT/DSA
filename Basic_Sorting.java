99ceeda768e188d57eab856f2bdd239ed34cc0c0
YusufHanzala <yusufhanzala456@gmail.com>
2026-06-07 09:40:49 +0530

    Bublle Insertion Selection Sort

--- Basic_Sorting.java
+++ Basic_Sorting.java
@@ -24,14 +24,25 @@ public class Basic_Sorting {
             arr[i] = temp;
         }
     }
+    public static void Insertion_Sort(int arr[]){
+        for(int i=1; i<arr.length; i++){
+            int current = arr[i];
+            int previous = i-1;
+            while(previous>= 0 && arr[previous] > current){
+                arr[previous+1] = arr[previous];
+                previous--;
+            }
+            arr[previous+1] = current;
+        }
+    }
     public static void printarr(int arr[]){
         for(int i=0; i<arr.length; i++){
             System.out.print(arr[i]+" ");
         }
     }
     public static void main(String args[]){
-        int arr[] = {1, 2, 6, 5, 4};
-        Selection_Sort(arr);
+        int arr[] = {1, 2, 3, 5, 4};
+        Insertion_Sort(arr);
         printarr(arr);
     }
 }
