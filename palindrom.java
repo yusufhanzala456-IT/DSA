e7e88a4099c7c5f75b1818cd20a19f11b8623527
YusufHanzala <yusufhanzala456@gmail.com>
2026-06-10 16:07:00 +0530

    Palindrom or Not

---
+++ palindrom.java
@@ -0,0 +1,15 @@
+public class palindrom {
+    public static boolean isPalindrom(String str){
+        for(int i=0; i<str.length()/2; i++){
+            int n = str.length();
+            if(str.charAt(i) != str.charAt(n-1-i)){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String args[]){
+        String str = "NOON";
+        System.out.print(isPalindrom(str));
+    }
+}
