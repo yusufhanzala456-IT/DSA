229b8490dcfcba4b5d123bb90cb0aff727f7745b
YusufHanzala <yusufhanzala456@gmail.com>
2026-06-12 11:18:12 +0530

    Remove duplicate value from sorted array

---
+++ RmvDuplcat.java
@@ -0,0 +1,39 @@
+public class RmvDuplcat {
+
+    public static int removeDuplicates(int[] nums) {
+
+        if (nums.length == 0) {
+            return 0;
+        }
+
+        int i = 0;
+
+        for (int j = 1; j < nums.length; j++) {
+
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+
+        return i + 1;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+
+        int k = removeDuplicates(nums);
+
+        System.out.println("Number of unique elements: " + k);
+
+        System.out.print("Array after removing duplicates: ");
+
+        for (int i = 0; i < k; i++) {
+            System.out.print(nums[i] + " ");
+        }
+
+        System.out.println();
+    }
+
+}
