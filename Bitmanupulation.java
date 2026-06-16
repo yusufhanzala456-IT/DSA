3a854ab4bdbb8f5de2088cd1a57874b033946874
YusufHanzala <yusufhanzala456@gmail.com>
2026-06-16 10:49:42 +0530

    All Bit Manipulation in one class

--- Bitmanupulation.java
+++ Bitmanupulation.java
@@ -38,6 +38,40 @@ public class Bitmanupulation {
         int bitMask = newBit<<i;
         return n|bitMask;
     }
+    public static int clearIBit(int n, int i){
+        int bitMask = (~0) << i;
+        return n & bitMask;
+    }
+    public static int clearBitsInRange(int n, int i, int j){
+        int a = ((~0) << (j+1));
+        int b = (1 << i) - 1;
+        int bitMask = a | b;
+        return n & bitMask;
+    }
+    public static boolean isPowerOfTwo(int n){
+        return (n & (n-1)) == 0;
+    }
+    public static int countSetsBit(int n){
+        int count = 0;
+        while(n > 0){
+            if((n & 1) != 0){
+                count ++;
+            }
+            n = n >> 1;
+        }
+        return count;
+    }
+    public static int fastPow(int a, int n){
+        int ans = 1;
+        while(n>0){
+            if((n & 1) != 0){
+                ans = ans * a;
+            }
+            a = a * a;
+            n = n>>1;
+        }
+        return ans;
+    }
     public static void main(String args[]){
         EvenOrOdd(3);
         EvenOrOdd(4);
@@ -47,6 +81,11 @@ public class Bitmanupulation {
         System.out.println(setIthBit(10, 2));
         System.out.println(clearIthBit(10, 1));
         System.out.println(updateIthBit(10, 2, 1));
-
+        System.out.println(clearIBit(15, 2));
+        System.out.println(clearBitsInRange(10, 2, 4));
+        System.out.println(isPowerOfTwo(8));
+        System.out.println(countSetsBit(15));
+        System.out.println(fastPow(3, 5));
+        
     }
 }
