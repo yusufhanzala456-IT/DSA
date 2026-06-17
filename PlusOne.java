c14fb3d2c1c718165c1b82d85fbbb0cb43cc00fe
YusufHanzala <yusufhanzala456@gmail.com>
2026-06-17 12:59:58 +0530

    Plus One In Last Digit Of Array

---
+++ PlusOne.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class PlusOne {
+
+    public static int[] plusOne(int[] digits) {
+
+        for(int i = digits.length - 1; i >= 0; i--) {
+
+            if(digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+
+            digits[i] = 0;
+        }
+
+        int[] result = new int[digits.length + 1];
+        result[0] = 1;
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        int[] digits = {9, 9, 9};
+
+        int[] answer = plusOne(digits);
+
+        System.out.println(Arrays.toString(answer));
+    }
+}
