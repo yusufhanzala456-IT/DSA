1bc1dbdc747555893656b83b92fc08f675777c79
YusufHanzala <yusufhanzala456@gmail.com>
2026-06-19 15:10:43 +0530

    Add Binary

---
+++ AddBinary.java
@@ -0,0 +1,40 @@
+public class AddBinary {
+
+    public static String addBinary(String a, String b) {
+
+        StringBuilder result = new StringBuilder();
+
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+
+        int carry = 0;
+
+        while(i >= 0 || j >= 0 || carry != 0) {
+
+            int sum = carry;
+
+            if(i >= 0) {
+                sum += a.charAt(i) - '0';
+                i--;
+            }
+
+            if(j >= 0) {
+                sum += b.charAt(j) - '0';
+                j--;
+            }
+
+            result.append(sum % 2);
+            carry = sum / 2;
+        }
+
+        return result.reverse().toString();
+    }
+
+    public static void main(String[] args) {
+
+        String a = "1010";
+        String b = "1011";
+
+        System.out.println("Sum = " + addBinary(a, b));
+    }
+}
